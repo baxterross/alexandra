fe470fde38311398f1c821a6004e34945a278db4
Baxter Ross <baxterross@gmail.com>
2016-06-28 17:35:08 -0700

    Changing homepage blurb

--- home.php
+++ home.php
@@ -1,7 +1,10 @@
 <div class="contentWrapper home" page="home">
 	<div class="contentInner">
 		<p>
-			 Alexandra Grabarchuk, PhD, is a Ukrainian-born musicologist and performer. She received her doctorate from UCLA in 2015, supported by an Alvin H. Johnson AMS 50 Dissertation Fellowship Award and a UCLA Dissertation Year Fellowship. Her forthcoming chapter on Soviet progressive rock will appear in <em>Prog Rock in Europe: Overview of a Persistent Musical Style</em> alongside the work of Allan Moore and Franco Fabbri. This edition will be published by Editions Universitaires de Dijon in 2016. She has also worked as a score reader for the Metropolitan Opera during their 2014-2016 seasons. In 2016, Alex joined the full-time staff of "counter-conventional" Pasadena-based orchestra <a href="http://muse-ique.com/about" target="_blank">MUSE/IQUE</a> as their Artistic Programs Manager.
+			Alexandra Grabarchuk, PhD, is a Ukrainian-born musicologist and performer.
+			She received her doctorate from UCLA in 2015, supported by an Alvin H. Johnson AMS 50 Dissertation Fellowship Award and a UCLA Dissertation Year Fellowship.
+			Her most recent publications include a chapter on Soviet progressive rock in <em>Prog Rock in Europe: Overview of a Persistent Musical Style</em> (2016), alongside the work of Allan Moore and Franco Fabbri.
+			In addition to teaching and performing in the Los Angeles area, she has also worked as a score reader for the Metropolitan Opera’s LA-based live HD broadcast team since 2014.
 		</p>
 		<div class="centerHolder">
 		  <a id="cv" class="download-link" href="<?php echo SITE; ?>/grabarchuk%20cv.pdf" target="_blank">curriculum vitae</a>
