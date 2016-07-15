10c6f039ed7077d64cb5f840fbed40ec3e6a67ef
Baxter Ross <baxterross@gmail.com>
2016-07-15 12:59:27 -0700

    ADding dallas opera link

--- music.php
+++ music.php
@@ -46,7 +46,7 @@
 					Alex has studied choral and orchestral conducting with Distinguished Professor of Conducting Donald Neuen, as well as Dr. James Smith and Dr. Arian Khaefi.
 					Alex has served as Managing Director & Conductor of the UCLA Early Music Ensemble, and worked as Director of Choral Activities at American Lutheran Church in Burbank.
 					She is currently the Assistant Conductor of the <a href="http://www.sgvccsingers.org/">San Gabriel Valley Choral Company</a>.
-					In 2015, she was an invited observer at the inaugural <a>Linda and Mitch Hart Institute for Women Conductors</a> at the Dallas Opera.
+					In 2015, she was an invited observer at the inaugural <a href="http://dallasopera.org/learn/womens-conducting-institute/">Linda and Mitch Hart Institute for Women Conductors</a> at the Dallas Opera.
 				  <?php if ($videos['conducting']) : ?>
 					<div class="centerHolder small">
 						<div class="arrow left">
